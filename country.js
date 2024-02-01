0510cb290f360f8ca2d769084a11130268d5c93c
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-02-01 05:17:00 +0300

    first commit

---
+++ country.js
@@ -0,0 +1,59 @@
+console.log("country.js");
+
+
+const getData = () =>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(response => response.json())
+    .then(country => explore(country))
+}
+
+getData()
+
+const explore = country => {
+    
+        const exploreContainer =  document.querySelector(".explore-Container")
+        // console.log(exploreContainer)
+        function processCountries(countries) {
+            return countries.map(country => {
+                return {
+                    name: country.name.common,
+                    capital: country.capital[0] || 'N/A', // Example property, customize as needed
+                    // Add more properties as needed
+                };
+            });
+        }
+        console.log(processCountries())
+
+
+            
+            console.log(get[0].name.common)
+            document.getElementById('search').addEventListener("click", function(){
+                const searchBox = document.getElementById('searchBox')
+
+
+            for(const countrys of country){   
+                    if(searchBox.value=== countrys.name.common ){
+                        const newDiv = document.createElement("div");
+                        newDiv.innerHTML = `
+                        <div class="my-2">
+                        <img class="h-36 w-64 mx-auto" src="${get[0].flags.png}" alt="">
+                        </div>
+        
+                            <h1 class="text-2xl text-center py-1"> Country Name : ${get[0].name.common}</h1>
+        
+                            <h3 class="font-medium">Official Name :${get[0].name.official}</h3>
+                            <p>Capital : </p>
+                            <p>Area :</p>
+                            <p>Border With : </p>
+                        `
+                        exploreContainer.appendChild(newDiv)
+                    }
+
+                }   
+
+
+        })
+
+}
+
+
---
+++ test.js
@@ -0,0 +1,13 @@
+
+const persons  = {
+           name:"jahid",
+           last: "ISlam",
+           age:27,
+           stydy : "developer"
+    }   
+
+    // console.log(persons.age)
+
+    for (const chk of persons){
+        console.log(chk)
+    }
